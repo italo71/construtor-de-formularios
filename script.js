b204f4d8bf8cae000802268a3645dac67bcf271d
Italo Braga <102165012+italo71@users.noreply.github.com>
2022-07-08 10:58:01 -0300

    Update bugs fixed

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-function alter_color_black(id, l, local, n) {
-    if (n) {
-        for (let i = 0; i <= 10; i++) {
-            document.getElementById(`${l}_${i}_${local}`).classList.remove('black');
-        }
-        document.getElementById(`${id}`).classList.add('black');
-    }
-    else {
-        for (let i = 0; i <= 4; i++) {
-            document.getElementById(`${l}_${i}_${local}`).classList.remove('black2');
-        }
-        document.getElementById(`${id}`).classList.add('black2');
-    }
-}
-
-function alter_color_gray(id, l, local) {
-    for (let i = 0; i <= 4; i++)
-        document.getElementById(`e_${i}_${local}`).classList.remove('gray');
-    document.getElementById(`${id}`).classList.add('gray');
-}
-
 function adicionarPergunta(tipo) {
     let finded = false;
     let local;
@@ -43,9 +22,13 @@ function adicionarPergunta(tipo) {
         }
     }
     if (tipo == ball5) {
-        for (let i2 = 0; i2 <= 10; i2++) {
-            document.getElementById(`f_${i2}`).id = `f_${i2}_${local}`;
-            document.getElementById(`f_${i2}_${local}`).setAttribute("onclick", `nota_fiveB = ${i2}; alter_color_black('f_${i2}_${local}', 'f', '${local}', false);`)
+        for (let i3 = 0; i3 <= 4; i3++) {
+            document.getElementById(`f_${i3}`).id = `f_${i3}_${local}`;
+            document.getElementById(`f_${i3}_${local}`).setAttribute("onclick", `nota_fiveB = ${i3}; alter_color_black('f_${i3}_${local}', 'f', '${local}', false);`)
+        }
+        for (let i = 1; i <= 2; i++) {
+            document.getElementById(`d_f_p_${i}`).id = `d_f_p_${i}_${local}`;
+            document.getElementById(`f_p_${i}`).id = `f_p_${i}_${local}`;
         }
     }
     if (tipo == radio5) {
@@ -54,6 +37,13 @@ function adicionarPergunta(tipo) {
             document.getElementById(`i_${i}`).id = `i_${i}_${local}`;
             document.getElementById(`l_r_${i}`).id = `l_r_${i}_${local}`;
         }
+    }
+    if (tipo == ball10) {
+        for (let i = 1; i <= 2; i++) {
+            document.getElementById(`n_p_${i}`).id = `n_p_${i}_${local}`;
+            document.getElementById(`d_n_p_${i}`).id = `d_n_p_${i}_${local}`
+        }
+        document.getElementById('n_ps').id = `n_ps_${local}`;
     }
 }
 
@@ -85,7 +75,127 @@ function btn_salvar_display() {
 function editar_pergunta(id) {
     let edit_pergunta;
     let i;
-    if (document.querySelector(`#q_${id} .radio5Q`) != undefined) {
+    if (document.querySelector(`#q_${id} .ball10Q`) != undefined) {
+        let insert_div;
+        if (document.querySelector(`#q_${id} .card .align-center div.m-t-5`) == undefined) {
+            edit_pergunta = document.querySelector(`#q_${id} .pergunta_editavel`).textContent;
+            document.querySelector(`#q_${id} .align-center`).innerHTML = input_editar_pergunta;
+            document.getElementById('pergunta_editada').value = edit_pergunta;
+            document.getElementById(`btn_e_${id}`).innerHTML = span_check;
+            document.getElementById(`btn_e_${id}`).style.backgroundColor = 'rgb(0, 202, 0)';
+            for (i = 1; i <= 2; i++) {
+                let last_parametro = document.getElementById(`n_p_${i}_${id}`).textContent;
+                document.getElementById(`d_n_p_${i}_${id}`).innerHTML = editar_parametro;
+                document.getElementById('idPadrao').id = `iParametro_${i}_${id}`;
+                document.getElementById(`iParametro_${i}_${id}`).value = last_parametro;
+                if (i == 1) {
+                    document.getElementById(`iParametro_${i}_${id}`).classList.add('n_p_l');
+                }
+                else if (i == 2) {
+                    document.getElementById(`iParametro_${i}_${id}`).classList.add('n_p_r');
+                }
+            }
+            for (i = 1; i <= 10; i++) {
+                if (i != id) {
+                    document.getElementById(`btn_e_${i}`).classList.add("none");
+                }
+                else {
+                    document.getElementById(`btn_r_${i}`).classList.add("none");
+                }
+            }
+        }
+
+        else {
+            edit_pergunta = document.querySelector(`#q_${id} .card .align-center .justify-content-center input#pergunta_editada`).value;
+            document.getElementById(`btn_e_${id}`).innerHTML = span_edit;
+            document.querySelector(`#q_${id} .card .align-center`).innerHTML = h3_pergunta;
+            document.querySelector(`#q_${id} .card .align-center h3.pergunta_editavel`).textContent = edit_pergunta;
+            document.getElementById(`btn_e_${id}`).style.backgroundColor = 'rgb(96,126,238)';
+            for (i = 1; i <= 2; i++) {
+                let last_parametro = document.getElementById(`iParametro_${i}_${id}`).value
+                document.getElementById(`d_n_p_${i}_${id}`).innerHTML = parametro_editado;
+                document.getElementById("parametro_editado").id = `n_p_${i}_${id}`;
+                document.getElementById(`n_p_${i}_${id}`).textContent = last_parametro;
+                if (i == 1) {
+                    document.getElementById(`n_p_${i}_${id}`).classList.add('n_p_l');
+                }
+                else if (i == 2) {
+                    document.getElementById(`n_p_${i}_${id}`).classList.add('n_p_r');
+                }
+            }
+            for (i = 1; i <= 10; i++) {
+                if (i != id)
+                    document.getElementById(`btn_e_${i}`).classList.remove("none");
+                else
+                    document.getElementById(`btn_r_${i}`).classList.remove("none");
+            }
+        }
+    }
+
+    else if (document.querySelector(`#q_${id} .ball5Q`) != undefined) {
+        if (document.querySelector(`#q_${id} .card .align-center div.m-t-5`) == undefined) {
+            edit_pergunta = document.querySelector(`#q_${id} .pergunta_editavel`).textContent;
+            document.querySelector(`#q_${id} .align-center`).innerHTML = input_editar_pergunta;
+            document.getElementById('pergunta_editada').value = edit_pergunta;
+            document.getElementById(`btn_e_${id}`).innerHTML = span_check;
+            document.getElementById(`btn_e_${id}`).style.backgroundColor = 'rgb(0, 202, 0)';
+            for (i = 1; i <= 2; i++) {
+                let last_parametro = document.getElementById(`f_p_${i}_${id}`).textContent;
+                document.getElementById(`d_f_p_${i}_${id}`).innerHTML = editar_parametro;
+                document.getElementById('idPadrao').id = `iParametro_${i}_${id}`;
+                document.getElementById(`iParametro_${i}_${id}`).value = last_parametro;
+                if (i == 1) {
+                    document.getElementById(`iParametro_${i}_${id}`).classList.add('n_p_l');
+                }
+                else if (i == 2) {
+                    document.getElementById(`iParametro_${i}_${id}`).classList.add('n_p_r');
+                }
+            }
+            for (i = 1; i <= 10; i++) {
+                if (i != id) {
+                    document.getElementById(`btn_e_${i}`).classList.add("none");
+                }
+                else {
+                    document.getElementById(`btn_r_${i}`).classList.add("none");
+                }
+            }
+        }
+
+
+        else {
+            edit_pergunta = document.querySelector(`#q_${id} .card .align-center .justify-content-center input#pergunta_editada`).value;
+            document.getElementById(`btn_e_${id}`).innerHTML = span_edit;
+            document.querySelector(`#q_${id} .card .align-center`).innerHTML = h3_pergunta;
+            document.querySelector(`#q_${id} .card .align-center h3.pergunta_editavel`).textContent = edit_pergunta;
+            document.getElementById(`btn_e_${id}`).style.backgroundColor = 'rgb(96,126,238)';
+            for (i = 1; i <= 2; i++) {
+                let last_parametro = document.getElementById(`iParametro_${i}_${id}`).value
+                document.getElementById(`d_f_p_${i}_${id}`).innerHTML = parametro_editado;
+                document.getElementById("parametro_editado").id = `f_p_${i}_${id}`;
+                document.getElementById(`f_p_${i}_${id}`).textContent = last_parametro;
+                if (i == 1) {
+                    document.getElementById(`f_p_${i}_${id}`).classList.add('n_p_l');
+                }
+                else if (i == 2) {
+                    document.getElementById(`f_p_${i}_${id}`).classList.add('n_p_r');
+                }
+            }
+            for (i = 1; i <= 10; i++) {
+                if (i != id)
+                    document.getElementById(`btn_e_${i}`).classList.remove("none");
+                else
+                    document.getElementById(`btn_r_${i}`).classList.remove("none");
+            }
+            for (i = 1; i <= 10; i++) {
+                if (i != id)
+                    document.getElementById(`btn_e_${i}`).classList.remove("none");
+                else
+                    document.getElementById(`btn_r_${i}`).classList.remove("none");
+            }
+        }
+    }
+
+    else if (document.querySelector(`#q_${id} .radio5Q`) != undefined) {
         if (document.querySelector(`#q_${id} .card .align-center div.m-t-5`) == undefined) {
             edit_pergunta = document.querySelector(`#q_${id} .pergunta_editavel`).textContent;
             document.querySelector(`#q_${id} .align-center`).innerHTML = input_editar_pergunta;
@@ -166,9 +276,10 @@ function editar_pergunta(id) {
 
 function salvar_questionario() {
     let i;
+    let i2;
     let pesquisa_personalizada;
-    for (i = 0; i <= 10; i++)
-        document.getElementById(`n_${i}`).classList.remove('black');
+
+    /* Obtem html do formulário */
     for (i = 1; i <= 10; i++) {
         pesquisa_personalizada = pesquisa_personalizada + document.getElementById(`q_${i}`).outerHTML;
     }
@@ -186,6 +297,12 @@ var input_editar_resposta = `<div class="justify-content-center m-t-5 m-b-5">
 <input type="text" name="" class="form-control" id="idPadrao">
 </div>`
 
+var editar_parametro = `<div class="m-t-5 m-b-5">
+<input type="text" name="" class="n_p_edit" id="idPadrao">
+</div>`
+
+var parametro_editado = `<span id="parametro_editado" class="n_p_l"></span>`
+
 var input_radio_padrao = `<input type="radio" id="iPadrao" value="0"
 name="i"><label for="0" id="idPadraoRadio"></label>`;
 
@@ -200,8 +317,8 @@ edit
 
 var h3_pergunta = `<h3 class="pergunta_editavel"></h3>`;
 
-var text = `<div class="card col-md-11 textQ">
-<div class="align-center">
+var text = `<div class="card col-md-12 textQ">
+<div class="align-center col-md-12">
     <h3 class="pergunta_editavel">Nos de sua opnião sobre nosso atendimento</h3>
 </div>
 <div class="col-md-12">
@@ -209,12 +326,12 @@ var text = `<div class="card col-md-11 textQ">
     </div>
 </div>`;
 
-var ball10 = `<div class="card col-md-11 align-items-center ball10Q">
-<div class="align-center">
+var ball10 = `<div class="card col-md-12 align-items-center ball10Q">
+<div class="align-center col-md-12">
     <h3 class="pergunta_editavel" id="nps_quest">Numa escala de 0 a 10, quanto você indicaria a <span id="nome_emp">**Nossa
             empresa**</span> a um amigo?</h3>
 </div>
-<div class="col-md-11">
+<div class="col-md-12">
     <div class="align-center" id="notas_nps">
         <div class="nota"><button class="btn" id="n_0">0</button></div>
         <div class="nota"><button class="btn" id="n_1">1</button></div>
@@ -227,20 +344,20 @@ var ball10 = `<div class="card col-md-11 align-items-center ball10Q">
         <div class="nota"><button class="btn" id="n_8">8</button></div>
         <div class="nota"><button class="btn" id="n_9">9</button></div>
         <div class="nota"><button class="btn" id="n_10">10</button></div>
-        <div>
-            <span id="n_p_1"><b>NÂO INDICARIA</b></span>
-            <span id="n_p_2"><b>INDICARIA</b></span>
+        <div id="n_ps">
+            <div id="d_n_p_1"><span id="n_p_1" class="n_p_l">NÂO INDICARIA</span></div>
+            <div id="d_n_p_2"><span id="n_p_2" class="n_p_r">INDICARIA</span></div>
         </div>
     </div>
 </div>
 </div>`;
 
-var ball5 = `<div class="card col-md-11 align-items-center ball5Q">
-<div class="align-center">
+var ball5 = `<div class="card col-md-12 align-items-center ball5Q">
+<div class="align-center col-md-12">
     <h3 class="pergunta_editavel" id="fiveB_quest">Avalie nossos serviço de 1 a 5</h3>
 </div>
-<div class="col-md-11">
-    <div class="align-center" id="notas_five">
+<div class="col-md-12">
+    <div class="align-center" id="">
         <div class="align-center" id="notas_five">
             <div class="nota_five"><button class="btn_five" id="f_0">1</button></div>
             <div class="nota_five"><button class="btn_five" id="f_1">2</button></div>
@@ -249,17 +366,17 @@ var ball5 = `<div class="card col-md-11 align-items-center ball5Q">
             <div class="nota_five"><button class="btn_five" id="f_4">5</button></div>
         </div>
         <div class="justify-content-center">
-            <div class="col-md-7">
-                <span id="f_p_1"><b>RUIM</b></span>
-                <span id="f_p_2"><b>EXELENTE</b></span>
+            <div class="col-md-8">
+                <div id="d_f_p_1" class="n_p_l"><span id="f_p_1">RUIM</span></div>
+                <div id="d_f_p_2" class="n_p_r"><span id="f_p_2">EXELENTE</span></div>
             </div>
         </div>
     </div>
 </div>
 </div>`;
 
-var radio5 = `<div class="card col-md-11 align-itens-center radio5Q">
-<div class="align-center">
+var radio5 = `<div class="card col-md-12 align-itens-center radio5Q">
+<div class="align-center col-md-12">
     <h3 class="pergunta_editavel" id="csat_quest">**Como você classificaria a *nossa empresa*
         sobre o valor total que a empresa
         oferece,
@@ -286,11 +403,11 @@ var radio5 = `<div class="card col-md-11 align-itens-center radio5Q">
 </div>
 </div>`;
 
-var radio2 = `<div class="card col-md-11 align-items-center radio2Q">
-<div class="align-center">
+var radio2 = `<div class="card col-md-12 align-items-center radio2Q maxW">
+<div class="align-center col-md-12 maxW">
     <h3 class="pergunta_editavel" id="double_quest">Vistoria foi aprovada</h3>
 </div>
-<div class="justify-content-center">
+<div class="justify-content-center col-md-12 maxW">
     <div class="col-md-12">
         <input type="radio" name="sim-nao" id=""><label for="">Sim</label><br>
         <input type="radio" name="sim-nao" id=""><label for="">Não</label><br>
@@ -298,8 +415,8 @@ var radio2 = `<div class="card col-md-11 align-items-center radio2Q">
 </div>
 </div>`;
 
-var emoji5 = `<div class="card col-md-11 align-itens-center emoji5Q">
-<div class="align-center">
+var emoji5 = `<div class="card col-md-12 align-itens-center emoji5Q">
+<div class="align-center col-md-12">
     <h3 class="pergunta_editavel" id="csat_quest">Qual a sua satisfação com o nosso atendimento?</h3>
 </div>
 <div class="col-md-12">
